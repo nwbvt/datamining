0acad9feaae6b1911d3b7f0244af57f45512a8a9
Nick Brown <nwbrown@gmail.com>
2015-02-12 18:20:02 -0500

    barely started

--- src/datamining/core.clj
+++ src/datamining/core.clj
@@ -1,5 +1,17 @@
 (ns datamining.core)
 
+(defn edges
+  "Gets the edges in a graph"
+  [graph]
+  (mapcat (fn [[start ends]] (map #(vec [start %]) ends)) graph))
+
+(defn- power-iteration
+  "Runs power iteration on a graph"
+  [graph epsilon]
+  (loop [prior graph]
+    )
+  )
+
 (defn page-rank
   "runs pagerank against a graph"
   [graph]
