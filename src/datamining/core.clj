c181c9de2dc3c152dea910880abf5c97eff81aaf
Nick Brown <nwbrown@gmail.com>
2015-02-12 20:51:39 -0500

    simple page rank working

--- src/datamining/core.clj
+++ src/datamining/core.clj
@@ -5,14 +5,38 @@
   [graph]
   (mapcat (fn [[start ends]] (map #(vec [start %]) ends)) graph))
 
+(defn rev-graph
+  "Reverses the graph"
+  [graph]
+  (let [edge-list (edges graph)]
+    (loop [rev-graph {} left edge-list]
+      (if (empty? left) rev-graph
+        (let [[in out] (first left)
+              in-list (if (contains? rev-graph out)
+                        (conj (rev-graph out) in) [in])]
+          (recur (assoc rev-graph out in-list) (rest left)))))))
+
+(defn map-dist
+  "'distance' between two maps
+   assumes same keyset"
+  [map1 map2]
+  (apply + (map #(Math/abs (- (double (map1 %)) (double (map2 %))))
+                (keys map1))))
+
 (defn- power-iteration
   "Runs power iteration on a graph"
   [graph epsilon]
-  (loop [prior graph]
-    )
-  )
+  (let [rev (rev-graph graph)
+        nodes (keys graph)]
+    (loop [prior (zipmap nodes (repeat (/ 1 (count nodes))))]
+      (let [scores (zipmap nodes
+                           (for [node nodes]
+                             (apply + (map #(/ (prior %) (count (graph %))) (rev node)))))]
+        (if (< epsilon (map-dist prior scores))
+          (recur scores)
+          scores))))) 
 
 (defn page-rank
   "runs pagerank against a graph"
   [graph]
-  )
+  (power-iteration graph 0.01))
--- test/datamining/core_test.clj
+++ test/datamining/core_test.clj
@@ -2,10 +2,17 @@
   (:require [clojure.test :refer :all]
             [datamining.core :refer :all]))
 
+(defn near
+  "Test if something is within a particular distance of another value"
+  ([val1 val2]
+   (near val1 val2 0.01))
+  ([val1 val2 epsilon]
+   (> epsilon (Math/abs (- (double val1) (double val2))))))
+
 (deftest pagerank-test
   (testing "Page rank"
     (let [g {:y [:y :a] :a [:y :m] :m [:a]}
           results (page-rank g)]
-      (is (= 2/5 (results :y)))
-      (is (= 2/5 (results :a)))
-      (is (= 1/5 (results :m))))))
+      (is (near 2/5 (results :y)))
+      (is (near 2/5 (results :a)))
+      (is (near 1/5 (results :m))))))
